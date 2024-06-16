a20e97bcdc1c2cd1ece378ab820634b92fda54b1
Nicolas Goualard <nicolas.goualard@sfr.fr>
2024-06-16 15:24:05 +0200

    Changed buffer structure

--- app/src/sensors/sensor_buffer.c
+++ app/src/sensors/sensor_buffer.c
@@ -5,19 +5,29 @@
 
 LOG_MODULE_REGISTER(sensor_buffer, CONFIG_APP_LOG_LEVEL);
 
-static imu_buffer_t acc_buffer = {.wr_idx = 0, .name = SESSION_ACC_FILE_NAME};
-static imu_buffer_t gyro_buffer = {.wr_idx = 0, .name = SESSION_GYRO_FILE_NAME};
+static imu_buffer_t acc_buffer = {.name = SESSION_ACC_FILE_NAME, .x = {.wr_idx = 0, .rd_idx = 0}, .y = {.wr_idx = 0, .rd_idx = 0}, .z = {.wr_idx = 0, .rd_idx = 0}, .ts = {.wr_idx = 0, .rd_idx = 0}};
+static imu_buffer_t gyro_buffer = {.name = SESSION_GYRO_FILE_NAME, .x = {.wr_idx = 0, .rd_idx = 0}, .y = {.wr_idx = 0, .rd_idx = 0}, .z = {.wr_idx = 0, .rd_idx = 0}, .ts = {.wr_idx = 0, .rd_idx = 0}};
 
 void sensor_buffer_put(imu_buffer_t *buf, struct bhy2_data_xyz data, uint64_t timestamp){
-	buf->data[0][buf->wr_idx] = data.x;
-	buf->data[1][buf->wr_idx] = data.y;
-	buf->data[2][buf->wr_idx] = data.z;
-	buf->timestamps[buf->wr_idx] = timestamp;
-	buf->wr_idx++;
+	buf->x.wr_buffer[buf->x.wr_idx++] = data.x;
+	buf->y.wr_buffer[buf->y.wr_idx++] = data.y;
+	buf->z.wr_buffer[buf->z.wr_idx++] = data.z;
+	buf->ts.wr_buffer[buf->ts.wr_idx++] = timestamp;
 
-	if (buf->wr_idx == IMU_SAMPLE_RATE){
-		LOG_WRN("%s buffer full, resetting wr_idx", buf->name);
-		buf->wr_idx = 0;
+	if (buf->ts.wr_idx == buf->x.wr_idx && buf->ts.wr_idx == buf->y.wr_idx && buf->ts.wr_idx == buf->z.wr_idx) {
+		if (buf->ts.wr_idx == IMU_SAMPLE_RATE) {
+			LOG_WRN("%s buffer full, resetting wr_idx and copying data to rd_buffer", buf->name);
+			buf->ts.wr_idx = 0;
+			buf->x.wr_idx = 0;
+			buf->y.wr_idx = 0;
+			buf->z.wr_idx = 0;
+			memcpy(buf.ts.rd_buffer, buf->ts.wr_buffer, IMU_SAMPLE_RATE * sizeof(uint64_t));
+			memcpy(buf.x.rd_buffer, buf->x.wr_buffer, IMU_SAMPLE_RATE * sizeof(int16_t));
+			memcpy(buf.y.rd_buffer, buf->y.wr_buffer, IMU_SAMPLE_RATE * sizeof(int16_t));
+			memcpy(buf.z.rd_buffer, buf->z.wr_buffer, IMU_SAMPLE_RATE * sizeof(int16_t));
+		}
+	} else {
+		LOG_WRN("Error incrementing write indexes");
 	}
 }
 
--- app/src/sensors/sensor_buffer.h
+++ app/src/sensors/sensor_buffer.h
@@ -4,9 +4,24 @@
 
 typedef struct {
 	uint8_t wr_idx;
-	int16_t data[3][IMU_SAMPLE_RATE]; // id 0 = x, id 1 = y, id 2 = z.
-	uint64_t timestamps[IMU_SAMPLE_RATE];
+	uint8_t rd_idx;
+	int16_t wr_buffer[IMU_SAMPLE_RATE];
+	int16_t rd_buffer[IMU_SAMPLE_RATE];
+} i16_buffer_t;
+
+typedef struct {
+	uint8_t wr_idx;
+	uint8_t rd_idx;
+	uint64_t wr_buffer[IMU_SAMPLE_RATE];
+	uint64_t rd_buffer[IMU_SAMPLE_RATE];
+} u64_buffer_t;
+
+typedef struct {
 	char* name;
+	i16_buffer_t x;
+	i16_buffer_t y;
+	i16_buffer_t z;
+	u64_buffer_t ts;
 } imu_buffer_t;
 
 void sensor_buffer_put(imu_buffer_t *buf, struct bhy2_data_xyz data, uint64_t timestamp);
